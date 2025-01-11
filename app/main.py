5797dcdaea6feffd044a19b4836e63eda067089f
Francesco Pallotta <work@francescopallotta.dev>
2025-01-11 22:01:55 +0100

    feat: added db initialization on startup to allow no conf docker image

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api.v1 import router as api_router
-from app.core.db import health as db_health
+from app.core.db import health as db_health, db
 from app.api.health import router as health_router
 
 
@@ -10,6 +10,7 @@ from app.api.health import router as health_router
 async def lifespan(_: FastAPI):
     if not db_health():
         raise Exception("Database is not healthy")
+    db.init_db()
     yield
 
 app = FastAPI(name="pinggy-server", lifespan=lifespan, redoc_url=None)
